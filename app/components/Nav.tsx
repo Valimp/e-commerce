dfb4b1be2bcddc3d98d5f5524caa24c708b4a066
Valimp <valentinchauvet.contact@gmail.com>
2024-01-26 20:04:51 +0100

    fix: make navbar responsive

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,14 +2,14 @@ import Link from "next/link"
 
 const Nav = () => {
   return (
-    <header className='flex justify-between items-center w-full bg-grass-green px-6 py-4 shadow'>
+    <header className='flex flex-col sm:flex-row gap-3 justify-between items-center w-full bg-grass-green px-6 py-4 shadow'>
         <h2>
             valimp
         </h2>
         <nav className="flex gap-6">
-            <Link href='/'>home</Link>
-            <Link href='/'>shop</Link>
-            <Link href='/'>contact</Link>
+            <Link href='/'>accueil</Link>
+            <Link href='/Shop'>nos offres</Link>
+            <Link href='/Contact'>nous contacter</Link>
         </nav>
     </header>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div><h1>Hello</h1></div>
+    <div><h1>Home</h1></div>
   );
 }
