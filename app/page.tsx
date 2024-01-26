c382fd9cccc21124c6c171ebce09b37423d7f61c
Valimp <valentinchauvet.contact@gmail.com>
2024-01-26 22:25:45 +0100

    feature: add main content to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
   return (
-    <div><h1>Home</h1></div>
+    <>
+      <main className="h-[45rem] flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-4 sm:gap-10">
+          <h1 className="relative text-6xl sm:text-8xl text-deep-green">valimp</h1>
+          <p className="text-2xl sm:text-4xl text-deep-green text-center">
+            La solution tech 
+              <span className="before:-z-10 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-sun-orange relative inline-block">
+                <span className="relative px-2"> sur mesure </span>
+              </span> 
+            pour votre entreprise
+          </p>
+        </div>
+      </main>
+      
+    </>
   );
 }
