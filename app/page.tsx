82bc709b31720f2c4a823e85d70263e0436c9e8c
Valimp <valentinchauvet.contact@gmail.com>
2024-01-27 10:48:58 +0100

    feature: main content on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
         <div className="flex flex-col justify-center items-center gap-4 sm:gap-10">
           <h1 className="relative text-6xl sm:text-8xl text-deep-green">valimp</h1>
           <p className="text-2xl sm:text-4xl text-deep-green text-center">
-            La solution tech 
+            Votre site e-commerce
               <span className="before:-z-10 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-sun-orange relative inline-block">
-                <span className="relative px-2"> sur mesure </span>
+                <span className="relative px-2"> eco-responsable </span>
               </span> 
-            pour votre entreprise
+            sur mesure
           </p>
         </div>
       </main>
