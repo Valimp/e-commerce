152583dc77d827f3d09c3a1b1e5c360ea8b9fa6f
Valimp <valentinchauvet.contact@gmail.com>
2024-01-27 10:47:51 +0100

    feature: active links colored

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,15 +1,20 @@
+"use client";
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const Nav = () => {
+
+  const pathname = usePathname();
+
   return (
     <header className='flex flex-col sm:flex-row gap-3 justify-between items-center w-full bg-grass-green px-6 py-4 shadow'>
         <h2>
             valimp
         </h2>
         <nav className="flex gap-6">
-            <Link href='/'>accueil</Link>
-            <Link href='/Shop'>nos offres</Link>
-            <Link href='/Contact'>nous contacter</Link>
+            <Link className={pathname === "/" ? "text-sun-orange":""} href='/'>accueil</Link>
+            <Link className={pathname === "/Shop" ? "text-sun-orange":""} href='/Shop'>nos offres</Link>
+            <Link className={pathname === "/Contact" ? "text-sun-orange":""} href='/Contact'>nous contacter</Link>
         </nav>
     </header>
   )
