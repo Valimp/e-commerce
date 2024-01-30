e5ce1b9034588a47eecf83e2d8e782992d68f347
Valimp <valentinchauvet.contact@gmail.com>
2024-01-30 23:35:27 +0100

    feature: SocialNetwork Badge v1

---
+++ app/components/BadgeLink.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BadgeLink = () => {
+  return (
+    <button className='w-10 h-10 bg-sun-orange rounded-full'>
+        
+    </button>
+  )
+}
+
+export default BadgeLink
