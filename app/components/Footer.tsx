a81bbf7a1beb7d51270facb99c047ea18426044a
Valimp <valentinchauvet.contact@gmail.com>
2024-01-31 00:12:36 +0100

    fix: footer bg-color more accessible

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,21 +7,21 @@ const Footer = () => {
   return (
     <div className='h-auto bg-deep-green flex items-start px-10 py-6 gap-24'>
         <nav className='flex flex-col'>
-            <h3 className='text-sun-orange text-lg'>Navigation</h3>
+            <h3 className='text-egg-white text-lg'>Navigation</h3>
             <Separator horizontal="15px" />
             <Link className='lowercase' href='/'>accueil</Link>
             <Link className='lowercase' href='/'>nos offres</Link>
             <Link className='lowercase' href='/'>nous contacter</Link>
         </nav>
         <div className='flex flex-col'>
-            <h3 className='text-sun-orange text-lg'>Nous contacter</h3>
+            <h3 className='text-egg-white text-lg'>Nous contacter</h3>
             <Separator horizontal="15px" />
             <Link className='lowercase' href='/'>page de contact</Link>
             <p className='lowercase text-egg-white text-sm' >valimp.contact@gmail.com</p>
             <p className='lowercase text-egg-white text-sm' >06 XX XX XX XX</p>
         </div>
         <div>
-            <h3 className='text-sun-orange text-lg'>Suivez-nous</h3>
+            <h3 className='text-egg-white text-lg'>Suivez-nous</h3>
             <Separator horizontal="15px" />
             <BadgeLink />
             
