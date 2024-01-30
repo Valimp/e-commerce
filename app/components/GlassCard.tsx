ea4417eea972cdc8035f0eebcced505acf451191
Valimp <valentinchauvet.contact@gmail.com>
2024-01-30 11:55:03 +0100

    fix: better glassCard and text changes on homepage

--- app/components/GlassCard.tsx
+++ app/components/GlassCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GlassCard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='w-4/5 h-auto sm:w-[50rem] sm:h-[24rem] bg-deep-green/10 rounded-3xl px-8 sm:px-16 py-10 backdrop-blur-lg'>
+    <div className='w-4/5 h-auto sm:w-[50rem] sm:h-[24rem] bg-egg-white/20 rounded-3xl px-8 sm:px-16 py-10 backdrop-blur-md'>
         { children }
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
     <>
       <UpButton />
       <main className="h-[42rem] flex justify-center items-center">
-        <div className="flex flex-col justify-center items-center gap-4 sm:gap-10 mx-2">
+        <div className="flex flex-col justify-center items-center gap-4 sm:gap-10 mx-2 px-24">
           <h1 className="relative text-6xl sm:text-8xl text-deep-green">valimp</h1>
           <p className="text-2xl sm:text-4xl text-deep-green text-center">
-            Votre site e-commerce
+            Votre ordinateur sur mesure et votre site web
               <span className="before:-z-10 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-sun-orange relative inline-block">
                 <span className="relative px-2"> eco-responsable </span>
               </span> 
-            sur mesure
+            respectueux de l'environnement.
           </p>
           <Link className="flex items-center shadow text-deep-green border hover:border-sun-orange rounded-xl px-3 py-2 border-deep-green text-lg sm:text-xl" href="/Contact">
             <FaPhoneAlt className="inline-block mr-4 animate-bounce" />
