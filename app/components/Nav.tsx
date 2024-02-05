9b4270b69b3ef2fd9d85f42eaee4c5b88ca8b6a7
Valimp <valentinchauvet.contact@gmail.com>
2024-02-05 14:14:24 +0100

    feature: logo implement

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -8,9 +8,10 @@ const Nav = () => {
 
   return (
     <header className='flex flex-col sm:flex-row gap-3 justify-between items-center w-full bg-grass-green px-6 py-4 shadow'>
-        <h2>
-            valimp
-        </h2>
+        <div className="flex items-center gap-5">
+          <img className="w-12 h-12" src="/images/logo.png" />
+          <h2>valimp</h2>
+        </div>
         <nav className="flex gap-6">
             <Link className={pathname === "/" ? "text-sun-orange":""} href='/'>accueil</Link>
             <Link className={pathname === "/Shop" ? "text-sun-orange":""} href='/Shop'>nos offres</Link>
